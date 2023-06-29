f96634c1bfe7e9bcc4a6ffcdcbd896dd155c4c5d
Shubham Kumar <ishubhamjnv@gmail.com>
2023-06-29 21:49:26 +0530

    navbar changes

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
   const handleClick = () => setNav(!nav);
 
   return (
-    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+    <div className="fixed inset-0 w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
       <div>
         <img src={logo} alt="Logo Image" style={{ width: "100px" }} />
       </div>
